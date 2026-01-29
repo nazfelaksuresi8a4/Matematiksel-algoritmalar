37a46bdbbde87d1d6067864c2cb951aef9b3b01b
Aslan akbey <agmroc@gmail.com>
2026-01-29 16:59:38 +0300

    Create dizideki_maksimumu_bulma.py

---
+++ dizideki_maksimumu_bulma.py
@@ -0,0 +1,16 @@
+def max_(dizi):
+    en_iyi_eleman_indeksi = 0
+    en_iyi_eleman = 0
+
+    for eleman in dizi:
+
+        if dizi.index(eleman) == 0:
+            elemanX = eleman
+        
+        elif eleman > elemanX:
+            elemanX = eleman
+
+    return elemanX
+            
+maksimum = max_([6,14,614,62,46,357,35])
+print(maksimum)
