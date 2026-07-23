1b27e6a66bbd34305d6f87b6e8b010717751110f
Aslan akbey <agmroc@gmail.com>
2026-07-23 07:19:50 +0300

    Update two_sum_problemi.py

--- two_sum_problemi.py
+++ two_sum_problemi.py
@@ -12,8 +12,8 @@ sonuc = bul(dizi,hedef)
 
 print(sonuc)
 
-#yukarıdaki çözüm yanlıştır çünkü 'in' 'dizi.index' gibi fonksiyonlar ve anahtarlar diziyi baştan sona gezer ve karmaşıklık O(n^2) olur 
-#diğer bir hata ise 1 den başlşayan döngü ve elemanların kendini görme riskidir 
+#yukarıdaki çözüm yanlıştır çünkü 'in' 'dizi.index' gibi fonksiyonlar ve anahtarlar diziyi baştan sona gezer ve karmaşıklık O(n^2) olur  aynı şeyi O(n^2) algoritmasında yaparsak ise bu sefer O(n^3) olup yine hatalı olurdu.
+#diğer bir hata ise 1 den başlşayan döngü ve elemanların kendini görme riskidir aynı 
 #doğru çözümn O(n) ve O(n^2) için aşşağıdaki gibidir 
 
 #O(n) çözüm 
